a52c725b5e3ebd51c9a15170c269d3e7a6b898d0
yamini692 <yaminithya692@gmail.com>
2024-10-01 13:50:11 +0530

    Your commit message

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#from users.models import CustomerUser   
 
-# Create your models here.
+class Class(models.Model): 
+    students = models.ManyToManyField(CustomerUser, related_name='enrolled_classes', limit_choices_to={'role': 'student'})
+    staff = models.ForeignKey(CustomerUser, on_delete=models.CASCADE, related_name="marks_recorded")
+    class_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.student} - {self.staff} - {self.class_name}"
