c78cbb11488b98aabd3ff053940f5f311bb639a8
yamini692 <yaminithya692@gmail.com>
2024-10-03 10:18:06 +0530

    updated on internal/views.py

--- internal/views.py
+++ internal/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import InternalMarks
+from .forms import InternalMarksForm
 
-# Create your views here.
+@login_required
+def add_internal_marks(request):
+    if request.method == "POST":
+        form = InternalMarksForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('marks_list')
+    else:
+        form = InternalMarksForm()
+    return render(request, 'internal/add_internal_marks.html', {'form': form})
+
+@login_required        
+def list_internal_marks(request):
+    marks = InternalMarks.objects.all()  # Fixed the typo here
+    return render(request, 'internal/list_internal_marks.html', {'marks': marks})
+
+@login_required
+def update_internal_marks(request, pk):  # Added 'pk' parameter
+    mark = get_object_or_404(InternalMarks, pk=pk)
+    
+    if request.method == 'POST':
+        form = InternalMarksForm(request.POST, instance=mark)  # Corrected form name
+        if form.is_valid():
+            form.save()
+            return redirect('marks_list')  # Fixed typo
+    else:
+        form = InternalMarksForm(instance=mark)
+    
+    return render(request, 'internal/update_internal_marks.html', {'form': form})
